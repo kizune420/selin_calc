76b7d6a50143eded2080a7c7457510f78526b0ed
kizune420 <pluxury1996@gmail.com>
2022-02-17 10:26:18 +0900

    Initial Commit

---
+++ selin_calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  selin_calc
+//
+//  Created by Тыкао on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
